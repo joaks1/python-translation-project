b068444005d6c5bea842261435f1e64e7cdc39c7
Jamie R. Oaks <joaks1@gmail.com>
2019-02-13 08:32:02 -0600

    Adding some main code to translate.py.

--- translate.py
+++ translate.py
@@ -1,5 +1,7 @@
 #! /usr/bin/env python3
 
+import sys
+
 def translate_sequence(rna_sequence, genetic_code):
     """Translates a sequence of RNA into a sequence of amino acids.
 
@@ -74,3 +76,27 @@ def get_longest_peptide(rna_sequence, genetic_code):
 
 if __name__ == '__main__':
     genetic_code = {'GUC': 'V', 'ACC': 'T', 'GUA': 'V', 'GUG': 'V', 'ACU': 'T', 'AAC': 'N', 'CCU': 'P', 'UGG': 'W', 'AGC': 'S', 'AUC': 'I', 'CAU': 'H', 'AAU': 'N', 'AGU': 'S', 'GUU': 'V', 'CAC': 'H', 'ACG': 'T', 'CCG': 'P', 'CCA': 'P', 'ACA': 'T', 'CCC': 'P', 'UGU': 'C', 'GGU': 'G', 'UCU': 'S', 'GCG': 'A', 'UGC': 'C', 'CAG': 'Q', 'GAU': 'D', 'UAU': 'Y', 'CGG': 'R', 'UCG': 'S', 'AGG': 'R', 'GGG': 'G', 'UCC': 'S', 'UCA': 'S', 'UAA': '*', 'GGA': 'G', 'UAC': 'Y', 'GAC': 'D', 'UAG': '*', 'AUA': 'I', 'GCA': 'A', 'CUU': 'L', 'GGC': 'G', 'AUG': 'M', 'CUG': 'L', 'GAG': 'E', 'CUC': 'L', 'AGA': 'R', 'CUA': 'L', 'GCC': 'A', 'AAA': 'K', 'AAG': 'K', 'CAA': 'Q', 'UUU': 'F', 'CGU': 'R', 'CGC': 'R', 'CGA': 'R', 'GCU': 'A', 'GAA': 'E', 'AUU': 'I', 'UUG': 'L', 'UUA': 'L', 'UGA': '*', 'UUC': 'F'}
+    rna_seq = ("AUG" +
+            "UAC" +
+            "UGG" +
+            "CAC" +
+            "GCU" +
+            "ACU" +
+            "GCU" +
+            "CCA" +
+            "UAU" +
+            "ACU" +
+            "CAC" +
+            "CAG" +
+            "AAU" +
+            "AUC" +
+            "AGU" +
+            "ACA" +
+            "GCG")
+    longest_peptide = get_longest_peptide(rna_sequence = rna_seq,
+            genetic_code = genetic_code)
+    assert isinstance(longest_peptide, str), "Oops: the longest peptide is {0}, not a string".format(longest_peptide)
+    message = "The longest peptide encoded by\n\t'{0}'\nis\n\t'{1}'\n".format(
+            rna_seq,
+            longest_peptide)
+    sys.stdout.write(message)
