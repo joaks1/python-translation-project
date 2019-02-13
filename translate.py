a69c7ba1a68034cb7e48cbb8a33cfd9c45cfe599
Jamie R. Oaks <joaks1@gmail.com>
2019-02-12 23:52:16 -0600

    Defining more placeholder functions.
    These currently do nothing, and will have to be coded for the exercise.

--- translate.py
+++ translate.py
@@ -7,9 +7,57 @@ def translate_sequence(rna_sequence, genetic_code):
     `genetic_code` given as a dict. Translation begins at the first position of
     the `rna_sequence` and continues until the first stop codon is encountered
     or the end of `rna_sequence` is reached.
+
+    If `rna_sequence` is less than 3 bases long, or starts with a stop codon,
+    an empty string is returned.
+    """
+    pass
+
+def get_all_translations(rna_sequence, genetic_code):
+    """Get a list of all amino acid sequences encoded in an RNA sequence.
+
+    All three reading frames of `rna_sequence` are scanned from 'left' to
+    'right', and the generation of a sequence of amino acids is started
+    whenever the start codon 'AUG' is found. The `rna_sequence` is assumed to
+    be in the correct orientation (i.e., no reverse and/or complement of the
+    sequence is explored).
+
+    The function returns a list of all possible amino acid sequences that
+    are encoded by `rna_sequence`.
+
+    If no amino acids can be translated from `rna_sequence`, an empty list is
+    returned.
+    """
+    pass
+
+def get_reverse(sequence):
+    """Reverse orientation of `sequence`.
+
+    Returns a string with `sequence` in the reverse order.
+
+    If `sequence` is empty, and empty string is returned.
+    """
+    pass
+
+def get_complement(sequence):
+    """Get the complement of `sequence`.
+
+    Returns a string with the complementary sequence of `sequence`.
+
+    If `sequence` is empty, and empty string is returned.
+    """
+    pass
+
+def reverse_and_complement(sequence):
+    """Get the reversed and complemented form of `sequence`.
+
+    Returns a string that is the reversed and complemented sequence
+    of `sequence`.
+
+    If `sequence` is empty, and empty string is returned.
     """
     pass
 
 
 if __name__ == '__main__':
-    pass
+    genetic_code = {'GUC': 'V', 'ACC': 'T', 'GUA': 'V', 'GUG': 'V', 'ACU': 'T', 'AAC': 'N', 'CCU': 'P', 'UGG': 'W', 'AGC': 'S', 'AUC': 'I', 'CAU': 'H', 'AAU': 'N', 'AGU': 'S', 'GUU': 'V', 'CAC': 'H', 'ACG': 'T', 'CCG': 'P', 'CCA': 'P', 'ACA': 'T', 'CCC': 'P', 'UGU': 'C', 'GGU': 'G', 'UCU': 'S', 'GCG': 'A', 'UGC': 'C', 'CAG': 'Q', 'GAU': 'D', 'UAU': 'Y', 'CGG': 'R', 'UCG': 'S', 'AGG': 'R', 'GGG': 'G', 'UCC': 'S', 'UCA': 'S', 'UAA': '*', 'GGA': 'G', 'UAC': 'Y', 'GAC': 'D', 'UAG': '*', 'AUA': 'I', 'GCA': 'A', 'CUU': 'L', 'GGC': 'G', 'AUG': 'M', 'CUG': 'L', 'GAG': 'E', 'CUC': 'L', 'AGA': 'R', 'CUA': 'L', 'GCC': 'A', 'AAA': 'K', 'AAG': 'K', 'CAA': 'Q', 'UUU': 'F', 'CGU': 'R', 'CGC': 'R', 'CGA': 'R', 'GCU': 'A', 'GAA': 'E', 'AUU': 'I', 'UUG': 'L', 'UUA': 'L', 'UGA': '*', 'UUC': 'F'}
