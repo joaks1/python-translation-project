2cf0a169bff1700d8a6f9fd15839059c971b4dea
Jamie R. Oaks <joaks1@gmail.com>
2019-02-12 22:08:29 -0600

    Adding initial script and unit tests.

---
+++ test_translate.py
@@ -0,0 +1,28 @@
+#! /usr/bin/env python3
+
+import unittest
+
+import translate
+
+class TestTranslateBaseClass(unittest.TestCase):
+    def setUp(self):
+        self.genetic_code = {'GUC': 'V', 'ACC': 'T', 'GUA': 'V', 'GUG': 'V', 'ACU': 'T', 'AAC': 'N', 'CCU': 'P', 'UGG': 'W', 'AGC': 'S', 'AUC': 'I', 'CAU': 'H', 'AAU': 'N', 'AGU': 'S', 'GUU': 'V', 'CAC': 'H', 'ACG': 'T', 'CCG': 'P', 'CCA': 'P', 'ACA': 'T', 'CCC': 'P', 'UGU': 'C', 'GGU': 'G', 'UCU': 'S', 'GCG': 'A', 'UGC': 'C', 'CAG': 'Q', 'GAU': 'D', 'UAU': 'Y', 'CGG': 'R', 'UCG': 'S', 'AGG': 'R', 'GGG': 'G', 'UCC': 'S', 'UCA': 'S', 'UAA': '*', 'GGA': 'G', 'UAC': 'Y', 'GAC': 'D', 'UAG': '*', 'AUA': 'I', 'GCA': 'A', 'CUU': 'L', 'GGC': 'G', 'AUG': 'M', 'CUG': 'L', 'GAG': 'E', 'CUC': 'L', 'AGA': 'R', 'CUA': 'L', 'GCC': 'A', 'AAA': 'K', 'AAG': 'K', 'CAA': 'Q', 'UUU': 'F', 'CGU': 'R', 'CGC': 'R', 'CGA': 'R', 'GCU': 'A', 'GAA': 'E', 'AUU': 'I', 'UUG': 'L', 'UUA': 'L', 'UGA': '*', 'UUC': 'F'}
+
+class TestTranslateSequence(TestTranslateBaseClass):
+    
+    def test_empty_rna_sequence(self):
+        rna_seq = ""
+        expected_amino_acids = ""
+        amino_acids = translate.translate_sequence(
+                rna_sequence = rna_seq,
+                genetic_code = self.genetic_code)
+        message = (
+                "Calling `translate_sequence` with `rna_sequence` '{0}'.\n"
+                "Expecting '{1}', but '{2}' was returned".format(
+                        rna_seq,
+                        expected_amino_acids,
+                        amino_acids))
+        self.assertEqual(amino_acids, expected_amino_acids, message)
+
+if __name__ == '__main__':
+    unittest.main() 
---
+++ translate.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python3
+
+def translate_sequence(rna_sequence, genetic_code):
+    """Translates a sequence of RNA into a sequence of amino acids.
+
+    Translates `rna_sequence` into string of amino acids, according to the
+    `genetic_code` given as a dict. Translation begins at the first position of
+    the `rna_sequence` and continues until the first stop codon is encountered
+    or the end of `rna_sequence` is reached.
+    """
+    pass
+
+
+if __name__ == '__main__':
+    pass
