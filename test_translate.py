e712d5f74a0b1ffdee1b8e170e467cd9e7a8c416
Jamie R. Oaks <joaks1@gmail.com>
2019-03-26 20:51:36 -0500

    Working on tests to improve failure messages.

--- test_translate.py
+++ test_translate.py
@@ -8,108 +8,105 @@ class TestTranslateBaseClass(unittest.TestCase):
     def setUp(self):
         self.genetic_code = {'GUC': 'V', 'ACC': 'T', 'GUA': 'V', 'GUG': 'V', 'ACU': 'T', 'AAC': 'N', 'CCU': 'P', 'UGG': 'W', 'AGC': 'S', 'AUC': 'I', 'CAU': 'H', 'AAU': 'N', 'AGU': 'S', 'GUU': 'V', 'CAC': 'H', 'ACG': 'T', 'CCG': 'P', 'CCA': 'P', 'ACA': 'T', 'CCC': 'P', 'UGU': 'C', 'GGU': 'G', 'UCU': 'S', 'GCG': 'A', 'UGC': 'C', 'CAG': 'Q', 'GAU': 'D', 'UAU': 'Y', 'CGG': 'R', 'UCG': 'S', 'AGG': 'R', 'GGG': 'G', 'UCC': 'S', 'UCA': 'S', 'UAA': '*', 'GGA': 'G', 'UAC': 'Y', 'GAC': 'D', 'UAG': '*', 'AUA': 'I', 'GCA': 'A', 'CUU': 'L', 'GGC': 'G', 'AUG': 'M', 'CUG': 'L', 'GAG': 'E', 'CUC': 'L', 'AGA': 'R', 'CUA': 'L', 'GCC': 'A', 'AAA': 'K', 'AAG': 'K', 'CAA': 'Q', 'UUU': 'F', 'CGU': 'R', 'CGC': 'R', 'CGA': 'R', 'GCU': 'A', 'GAA': 'E', 'AUU': 'I', 'UUG': 'L', 'UUA': 'L', 'UGA': '*', 'UUC': 'F'}
 
+    def get_failure_message(self, function, key_word_args, expected_result,
+            result):
+        message = (
+                "\n\n"
+                "Calling `{function_name}` with the following parameters:\n".format(
+                        function_name = function.__name__)
+                )
+        for parameter_name, parameter_value in key_word_args.items():
+            message += "\t{parameter_name} = {parameter_value!r}    {parameter_type}\n".format(
+                    parameter_name = parameter_name,
+                    parameter_value = parameter_value,
+                    parameter_type = type(parameter_value))
+        message += (
+                "Expecting\n"
+                "\t{expected_result!r}    {expected_type}\n"
+                "to be returned, but got\n"
+                "\t{result!r}    {returned_type}\n".format(
+                        expected_result = expected_result,
+                        expected_type = type(expected_result),
+                        result = result,
+                        returned_type = type(result))
+                )
+        return message
+
+    def run_test_of_function(self, function, key_word_args, expected_result):
+        result = function(**key_word_args)
+        self.assertEqual(result, expected_result,
+                self.get_failure_message(
+                        function = function,
+                        key_word_args = key_word_args,
+                        expected_result = expected_result,
+                        result = result))
+
     def run_translate_sequence(self, rna_seq,
             expected_amino_acid_seq,
             gen_code = None):
         if gen_code is None:
             gen_code = self.genetic_code
-        amino_acid_seq = translate.translate_sequence(
-                rna_sequence = rna_seq,
-                genetic_code = gen_code)
-        message = (
-                "\n\n"
-                "Calling `translate_sequence` with `rna_sequence`:\n"
-                "\t{0!r}\n"
-                "Expecting {1!r}, but {2!r} was returned".format(
-                        rna_seq,
-                        expected_amino_acid_seq,
-                        amino_acid_seq))
-        self.assertEqual(amino_acid_seq, expected_amino_acid_seq, message)
+        key_word_args = {
+                "rna_sequence" : rna_seq,
+                "genetic_code" : gen_code,
+                }
+        self.run_test_of_function(
+                function = translate.translate_sequence,
+                key_word_args = key_word_args,
+                expected_result = expected_amino_acid_seq)
 
     def run_get_all_translations(self, rna_seq,
-            expected_results,
+            expected_result,
             gen_code = None):
         if gen_code is None:
             gen_code = self.genetic_code
-        amino_acid_seqs = translate.get_all_translations(
-                rna_sequence = rna_seq,
-                genetic_code = gen_code)
-        message = (
-                "\n\n"
-                "Calling `get_all_translations` with `rna_sequence`:\n"
-                "\t{0!r}\n"
-                "Expecting {1!r}, but {2!r} was returned".format(
-                        rna_seq,
-                        expected_results,
-                        amino_acid_seqs))
+        key_word_args = {
+                "rna_sequence" : rna_seq,
+                "genetic_code" : gen_code,
+                }
+        amino_acid_seqs = translate.get_all_translations(**key_word_args)
         if amino_acid_seqs:
             amino_acid_seqs = sorted(amino_acid_seqs)
-        if expected_results:
-            expected_results = sorted(expected_results)
-        self.assertEqual(amino_acid_seqs, expected_results, message)
+        if expected_result:
+            expected_result = sorted(expected_result)
+        self.assertEqual(amino_acid_seqs, expected_result,
+                self.get_failure_message(
+                        function = translate.get_all_translations,
+                        key_word_args = key_word_args,
+                        expected_result = expected_result,
+                        result = amino_acid_seqs))
 
     def run_get_reverse(self, seq, expected_result):
-        rev_seq = translate.get_reverse(seq)
-        message = (
-                "\n\n"
-                "Calling `get_reverse` with argument:\n"
-                "\t{0!r}\n"
-                "Expecting\n"
-                "\t{1!r}\n"
-                "to be returned, but got\n"
-                "\t{2!r}\n".format(
-                        seq,
-                        expected_result,
-                        rev_seq))
-        self.assertEqual(rev_seq, expected_result, message)
+        self.run_test_of_function(
+                function = translate.get_reverse,
+                key_word_args = {"sequence" : seq},
+                expected_result = expected_result)
 
     def run_get_complement(self, seq, expected_result):
-        comp_seq = translate.get_complement(seq)
-        message = (
-                "\n\n"
-                "Calling `get_complement` with argument:\n"
-                "\t{0!r}\n"
-                "Expecting\n"
-                "\t{1!r}\n"
-                "to be returned, but got\n"
-                "\t{2!r}\n".format(
-                        seq,
-                        expected_result,
-                        comp_seq))
-        self.assertEqual(comp_seq, expected_result, message)
+        self.run_test_of_function(
+                function = translate.get_complement,
+                key_word_args = {"sequence" : seq},
+                expected_result = expected_result)
 
     def run_reverse_and_complement(self, seq, expected_result):
-        rc_seq = translate.reverse_and_complement(seq)
-        message = (
-                "\n\n"
-                "Calling `reverse_and_complement` with argument:\n"
-                "\t{0!r}\n"
-                "Expecting\n"
-                "\t{1!r}\n"
-                "to be returned, but got\n"
-                "\t{2!r}\n".format(
-                        seq,
-                        expected_result,
-                        rc_seq))
-        self.assertEqual(rc_seq, expected_result, message)
+        self.run_test_of_function(
+                function = translate.reverse_and_complement,
+                key_word_args = {"sequence" : seq},
+                expected_result = expected_result)
 
     def run_get_longest_peptide(self, rna_seq,
             expected_result,
             gen_code = None):
         if gen_code is None:
             gen_code = self.genetic_code
-        amino_acid_seq = translate.get_longest_peptide(
-                rna_sequence = rna_seq,
-                genetic_code = gen_code)
-        message = (
-                "\n\n"
-                "Calling `get_longest_peptide` with `rna_sequence`:\n"
-                "\t{0!r}\n"
-                "Expecting {1!r}, but {2!r} was returned".format(
-                        rna_seq,
-                        expected_result,
-                        amino_acid_seq))
-        self.assertEqual(amino_acid_seq, expected_result, message)
+        key_word_args = {
+                "rna_sequence" : rna_seq,
+                "genetic_code" : gen_code,
+                }
+        self.run_test_of_function(
+                function = translate.get_longest_peptide,
+                key_word_args = key_word_args,
+                expected_result = expected_result)
 
 
 class TestTranslateSequence(TestTranslateBaseClass):
@@ -204,14 +201,14 @@ class TestGetAllTranslations(TestTranslateBaseClass):
         expected_amino_acid_seqs = []
         self.run_get_all_translations(
                 rna_seq = rna_seq,
-                expected_results = expected_amino_acid_seqs)
+                expected_result = expected_amino_acid_seqs)
 
     def test_one_start_at_end(self):
         rna_seq = "GUCGAAUAACGAAUG"
         expected_amino_acid_seqs = ["M"]
         self.run_get_all_translations(
                 rna_seq = rna_seq,
-                expected_results = expected_amino_acid_seqs)
+                expected_result = expected_amino_acid_seqs)
 
     def test_two_short_peptides(self):
         rna_seq = "CCUGAAUGACGUACGUAUGACUGCAGUACGUUACGUACG"
@@ -221,7 +218,7 @@ class TestGetAllTranslations(TestTranslateBaseClass):
                 ]
         self.run_get_all_translations(
                 rna_seq = rna_seq,
-                expected_results = expected_amino_acid_seqs)
+                expected_result = expected_amino_acid_seqs)
 
     def test_two_short_peptides_lower_case(self):
         rna_seq = "ccugaaugacguacguaugacugcaguacguuacguacg"
@@ -231,7 +228,7 @@ class TestGetAllTranslations(TestTranslateBaseClass):
                 ]
         self.run_get_all_translations(
                 rna_seq = rna_seq,
-                expected_results = expected_amino_acid_seqs)
+                expected_result = expected_amino_acid_seqs)
 
     def test_two_short_peptides_at_beginning(self):
         rna_seq = "AUGACGUACGUAUGACUGCAGUACGUUACGUACG"
@@ -241,7 +238,7 @@ class TestGetAllTranslations(TestTranslateBaseClass):
                 ]
         self.run_get_all_translations(
                 rna_seq = rna_seq,
-                expected_results = expected_amino_acid_seqs)
+                expected_result = expected_amino_acid_seqs)
 
 
 class TestGetReverse(TestTranslateBaseClass):
